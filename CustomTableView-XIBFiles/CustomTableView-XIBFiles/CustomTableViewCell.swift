090f08fc236a3cd71ec2a68a821b23391fa9aeab
Martin Vacas <martinvacas@MacBook-Pro-de-infra.local>
2017-09-08 12:20:35 -0300

    Swift Table Views Implementations

---
+++ CustomTableView-XIBFiles/CustomTableView-XIBFiles/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  CustomTableView-XIBFiles
+//
+//  Created by Martin Vacas on 8/9/17.
+//  Copyright © 2017 Martin Vacas. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageCell: UIImageView!
+
+    @IBOutlet var labelCell: UILabel!
+}
--- CustomTableView-XIBFiles/CustomTableView-XIBFiles/CustomTableViewController.swift
+++ CustomTableView-XIBFiles/CustomTableView-XIBFiles/CustomTableViewController.swift
@@ -10,32 +10,36 @@ import UIKit
 
 class CustomTableViewController: UITableViewController {
 
-    @IBOutlet var myTableView: UITableView!
     
+    @IBOutlet var myTableView: UITableView!
     let marcas = ["Audi", "Ferrari", "BMW", "Porsche"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        myTableView.register( UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomTableViewCell")
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return marcas.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
         // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell", for: indexPath) as! CustomTableViewCell
+        let marca = marcas[indexPath.row]
+        cell.labelCell.text = marca
+        cell.imageCell.image = UIImage(named: marca)
 
         return cell
     }
